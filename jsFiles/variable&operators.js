3d734eeb9799e27f675ed616abef4394c0363a81
Fox <alexandru.pricop@bestvalue.eu>
2025-06-23 15:46:15 +0300

    Adding first files

---
+++ jsFiles/variable&operators.js
@@ -0,0 +1,17 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 25;
+console.log(age);
+
+age = 56;
+
+console.log(age);
+
+const pi = 3.14;
+// pi = 10;
+
+console.log(pi);
